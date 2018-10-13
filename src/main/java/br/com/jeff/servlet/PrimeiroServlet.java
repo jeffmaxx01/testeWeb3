997441cc1c847b299ecca76f35b6fb1793659fda
jeffmaxx01 <jeferson.fv@gmail.com>
2018-10-13 19:19:08 -0300

    inicial

---
+++ src/main/java/br/com/jeff/servlet/PrimeiroServlet.java
@@ -0,0 +1,22 @@
+package br.com.jeff.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns= {"/jeff"})
+public class PrimeiroServlet  extends HttpServlet {
+
+	
+	
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+}
